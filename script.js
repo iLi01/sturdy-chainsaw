4b8dc77d2ecddbc6a522c2d8049df873f33891d8
[C[B[A[A[A[A[D[C[C[C[C[C[C[C[C[C[CiLi01[D[D[C[C[C[B[D[D[D[D[D[D[D[D[D[D[Cilinca.pricop100@gmail.com[C[B[C[C[C[C[C[C[C <ilinca.pricop100@gmail.com>
2024-05-02 20:53:35 +0100

    scroll snap and main skeleton

--- script.js
+++ script.js
@@ -5,3 +5,26 @@ const nav = document.getElementById("nav-bar");
 button.addEventListener('click', () => {
   nav.classList.toggle('open');
 });
+
+
+//slideshow of the project cards
+const arrowRight = document.getElementById("arrow-right-div");
+const arrowLeft = document.getElementById("arrow-left-div");
+const secondCards = document.getElementById("second-page-cards");
+const firstCards = document.getElementById("first-page-cards");
+
+var clicked = false;
+
+arrowRight.addEventListener('click', () => {
+  secondCards.classList.toggle('open');
+  firstCards.classList.toggle('open');
+  clicked = true;
+}, {once : false});
+
+arrowLeft.addEventListener('click', () => {
+  secondCards.classList.toggle('open');
+  firstCards.classList.toggle('open');
+  clicked = true;
+}, {once : false});
+//how to make it go through only back and forth?
+//once - true if i want it to be a singular eventlistener
